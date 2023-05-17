6915e213c5211043524a6c2502b67e28eb0f0ef5
fahime abouhamze <abouhamze.fahime@gmail.com>
2023-05-17 21:02:53 +0330

    commit changes

---
+++ ConsoleApp1/Entities/DbStorage.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleApp1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Entities
+{
+    public class DbStorage
+    {
+        public List<Person> people = new List<Person>(){
+            new Person() { Id = 1 , Name="Zahra" , Nationalcode="adsfasfd"},
+            new Person() { Id = 2 ,Name = "fariba" , Nationalcode ="asdfaf"},
+            new Person() { Id = 3 ,Name="Zhina" , Nationalcode ="afdasfas"}
+    };
+    }
+}
